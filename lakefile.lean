f6fd1b78f454e4fcfae2f4fd7c3c836a0ab92af4
Adam Topaz <github@adamtopaz.com>
2022-10-06 15:54:43 -0600

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package univAlg {
+  -- add package configuration options here
+}
+
+@[defaultTarget]
+lean_lib UnivAlg {
+  -- add library configuration options here
+}
